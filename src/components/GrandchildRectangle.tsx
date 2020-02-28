aaf862613c2d0d816c3b8a6aa871c229bd6ffa40
Christian Padilla <christianepadilla@gmail.com>
2020-02-28 03:55:36 -0800

    Add some documentation and a TODO to this cryptic component

--- src/components/GrandchildRectangle.tsx
+++ src/components/GrandchildRectangle.tsx
@@ -4,6 +4,9 @@ import { Rect, Group } from 'react-konva';
 import { ComponentsInt, ComponentInt, ChildInt } from '../utils/interfaces';
 
 // ** this file might restrict you from making the child of a component one of its references - prevents circular references
+// (no cycle detection here)
+// that is guarded against, as far as I can tell, only on lines 138/139 of LeftColExpansionPanel.tsx
+// (that component is in desperate need of a rename and a refactor, btw)
 
 interface PropsInt {
   x: number;
@@ -115,11 +118,21 @@ class GrandchildRectangle extends Component<PropsInt, StateInt> {
           strokeScaleEnabled={false}
           draggable={false}
         />
+        // The user can arbitrarily nest Rectangles (components) inside one another
+        // we need some way to deal with arbitrary nesting... recursion!
+        // (ie we'll have GrandchileRectangle call its own createElement() from within its own render())
+        // But, what should the base case be? A "leaf" Rectangle, one without children.
+        // If the Rectangle we're in has an empty children array, the map call will just return an empty array
+        // and recursion will happily stop.
+        // (Additionally, we have 'HTML' components for the user to add. The pure HTML components should not
+        // have children of their own, so don't bother looking, hence the childType check)
+        // TODO: is GrandchildRectangle even required? Maybe this recursion could happen in Rectangle?
         {childType === 'COMP' &&
           components
             .find((comp: ComponentInt) => comp.title === childComponentName)
             .children.filter((child: ChildInt) => child.childId !== -1)
             .map((grandchild: ChildInt, i: number) => (
+              // The recursive call itself:
               <GrandchildRectangle
                 key={i}
                 components={components}
@@ -132,6 +145,9 @@ class GrandchildRectangle extends Component<PropsInt, StateInt> {
                 childComponentId={grandchild.childComponentId}
                 focusChild={focusChild}
                 childId={childId}
+                // the below size/position scaling was non-trivial to figure out!
+                // A given Rectangle needs to be scaled to the size and position of its parent
+                // its parent is actually the pseudoChild of this. Terrible naming, I know
                 width={
                   grandchild.position.width *
                   (width / this.getPseudoChild().position.width)
